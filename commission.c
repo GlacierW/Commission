b8a609e256c6e9fac9bf2d09758c547d3526a45d
GlacierW <glacier314@gmail.com>
2014-10-21 02:26:26 +0800

    1. unify the date type to double to avoid type conversion error 2. add functionality to calculate the commission

--- commission.c
+++ commission.c
@@ -1,9 +1,14 @@
 #include "commission.h"
 
-const int	 SALE_RANK_THRESHOLD[SALE_RANKS] = { 0, 1000, 1800 };
+const double SALE_RANK_THRESHOLD[SALE_RANKS] = { 0.0, 1000.0, 1800.0 };
 const double SALE_COMMISSION_PERCENT[SALE_RANKS] = { 0.1, 0.15, 0.2 };
 
 double get_commission_month( int nb_lock, int nb_stock, int nb_barrel ) {
+	
+	int i;
+	
+	double sale = 0.0,
+		   commission = 0.0;
 	
 	// validate the input sale
 	if(
@@ -17,5 +22,20 @@ double get_commission_month( int nb_lock, int nb_stock, int nb_barrel ) {
 		return ERR_INVALID_SALE;
 	}
 	
-	return (double)0xdeadbeef;
+	sale = PRICE_LOCK   * nb_lock 
+		 + PRICE_STOCK  * nb_stock
+		 + PRICE_BARREL * nb_barrel;
+		
+	// decide rank 
+	for( i = SALE_RANKS - 1; i >= 0; --i ) 
+		if( sale > SALE_RANK_THRESHOLD[i] ) 
+			break;
+			
+	// calculate commission
+	for( ; i >= 0; --i ) {
+		commission = commission + ((sale - SALE_RANK_THRESHOLD[i]) * SALE_COMMISSION_PERCENT[i]);
+		sale = SALE_RANK_THRESHOLD[i];
+	}
+	
+	return commission;
 }
--- commission.h
+++ commission.h
@@ -1,9 +1,9 @@
 #ifndef __COMMISION_H__
 #define __COMMISION_H__
 
-#define PRICE_LOCK		45
-#define PRICE_STOCK		30
-#define PRICE_BARREL	25
+#define PRICE_LOCK		45.0
+#define PRICE_STOCK		30.0
+#define PRICE_BARREL	25.0
 
 #define NB_LOCK_MIN		1
 #define NB_LOCK_MAX		70
@@ -18,7 +18,7 @@
 
 #define ERR_INVALID_SALE 0.0
 
-extern const int	SALE_RANK_THRESHOLD[SALE_RANKS];
+extern const double	SALE_RANK_THRESHOLD[SALE_RANKS];
 extern const double	SALE_COMMISSION_PERCENT[SALE_RANKS];
 
 extern double get_commission_month( int nb_lock, int nb_stock, int nb_barrel );
