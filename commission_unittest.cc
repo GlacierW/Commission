d59e7ca2b3ca2601747ad254a608c8be714984dc
GlacierW <glacier314@gmail.com>
2014-10-21 01:58:27 +0800

    add testing on negative sale

--- commission_unittest.cc
+++ commission_unittest.cc
@@ -6,3 +6,17 @@ TEST(CommisionMonthTest, NormalBoundarySale ) {
 	EXPECT_DOUBLE_EQ( 1420.0, get_commission_month(NB_LOCK_MAX, NB_STOCK_MAX, NB_BARREL_MAX) );
 }
 
+TEST(CommisionMonthTest, NegativeSale ) {
+	// one negative
+	EXPECT_DOUBLE_EQ( ERR_INVALID_SALE, get_commission_month(-1, 1, 1) );
+	EXPECT_DOUBLE_EQ( ERR_INVALID_SALE, get_commission_month(1, -1, 1) );
+	EXPECT_DOUBLE_EQ( ERR_INVALID_SALE, get_commission_month(1, 1, -1) );
+	
+	// two negative
+	EXPECT_DOUBLE_EQ( ERR_INVALID_SALE, get_commission_month(-1, -1, 1) );
+	EXPECT_DOUBLE_EQ( ERR_INVALID_SALE, get_commission_month(-1, 1, -1) );
+	EXPECT_DOUBLE_EQ( ERR_INVALID_SALE, get_commission_month(1, -1, -1) );
+	
+	// all negative
+	EXPECT_DOUBLE_EQ( ERR_INVALID_SALE, get_commission_month(-1, -1, -1) );
+}
