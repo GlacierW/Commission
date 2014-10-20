15b4bab2cbf7977509306cb550dbd49d15f28e1e
GlacierW <glacier314@gmail.com>
2014-10-21 01:57:50 +0800

    add testing on the boundary sale

--- commission_unittest.cc
+++ commission_unittest.cc
@@ -1,4 +1,8 @@
 #include <gtest/gtest.h>
 #include "commission.h"
 
+TEST(CommisionMonthTest, NormalBoundarySale ) {
+	EXPECT_DOUBLE_EQ( 10.0, get_commission_month(1, 1, 1) );
+	EXPECT_DOUBLE_EQ( 1420.0, get_commission_month(NB_LOCK_MAX, NB_STOCK_MAX, NB_BARREL_MAX) );
+}
 
